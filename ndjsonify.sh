d392eda5dd2986bf0045da270e230db8de19c760
Jordan Verasamy <j.verasamy@gmail.com>
2019-08-22 11:32:29 -0400

    do replay conversion, update ndjsonify script

--- ndjsonify.sh
+++ ndjsonify.sh
@@ -4,4 +4,5 @@ yourfilenames=`ls ./replays/sc2replay/*.SC2Replay`
 for eachfile in $yourfilenames
 do
    python ../s2protocol/s2protocol/s2_cli.py --all --ndjson $eachfile > "$eachfile.ndjson"
+   echo "."
 done
