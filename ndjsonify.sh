18579d944c22bc95fdb001e67c5cf504823e4c40
Jordan Verasamy <j.verasamy@gmail.com>
2019-08-22 11:31:08 -0400

    remove spaces from replay names

--- ndjsonify.sh
+++ ndjsonify.sh
@@ -3,5 +3,5 @@
 yourfilenames=`ls ./replays/sc2replay/*.SC2Replay`
 for eachfile in $yourfilenames
 do
-   python ../s2protocol/s2protocol/s2_cli.py --all --ndjson $eachfile
+   python ../s2protocol/s2protocol/s2_cli.py --all --ndjson $eachfile > "$eachfile.ndjson"
 done
